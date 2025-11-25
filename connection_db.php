7fc4431dc1bc1e3df998bda48368e8b839683422
JennifferZhigue <jmzhigue@utpl.edu.ec>
2025-11-25 16:55:28 -0500

    Update connection_db.php

--- connection_db.php
+++ connection_db.php
@@ -5,7 +5,7 @@
 define('DB_SERVIDOR', 'sql210.infinityfree.com'); 
 define('DB_USUARIO', 'if0_40493778'); 
 define('DB_CONTRASENA','XPiA4k3zO69'); 
-define('DB_NOMBRE', 'if0_40493778_XXX'); 
+define('DB_NOMBRE', 'if0_40493778_information'); 
 
 // Intentar establecer la conexión
 $datos_conex = new mysqli(DB_SERVIDOR, DB_USUARIO, DB_CONTRASENA, DB_NOMBRE);
@@ -17,5 +17,6 @@ if ($datos_conex->connect_error) {
 
 // Establecer el conjunto de caracteres a UTF-8 para evitar problemas de acentos y eñes
 $datos_conex->set_charset("utf8");
+
 
 ?>
